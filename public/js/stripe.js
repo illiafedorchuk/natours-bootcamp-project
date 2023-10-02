8d9cbd1314fc85720c23f4604d3b5613b2766706
illiafedorchuk <illya4study@gmail.com>
2023-10-02 10:49:30 +0300

    Preapared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,10 +6,8 @@ const stripe = Stripe('pk_test_51NvmLPCBkuAX7l1KV6wNcC1wV3U0nlIb5WvknJ55zFiqAUsf
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
     // 2) Create checkout form + chanre credit card
     await stripe.redirectToCheckout({
